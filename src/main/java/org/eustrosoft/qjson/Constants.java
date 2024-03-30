7850fb8368b0801c9feff6b78f50dabdd36bf99e
Yadzuka <yadzuka@eustrosoft.org>
2024-03-30 20:32:59 +0300

    Added json parser and QJson as helper to json creation

---
+++ src/main/java/org/eustrosoft/qjson/Constants.java
@@ -0,0 +1,18 @@
+package org.eustrosoft.qjson;
+
+public final class Constants {
+    public final static String NULL = "null";
+    public final static String START_OBJ = "{";
+    public final static String END_OBJ = "}";
+    public final static String START_ARR = "[";
+    public final static String END_ARR = "]";
+
+    public final static String EMPTY_ARR = "[]";
+    public final static String EMPTY_OBJ = "{}";
+
+    public final static String COMMA = ",";
+
+    private Constants() {
+
+    }
+}
